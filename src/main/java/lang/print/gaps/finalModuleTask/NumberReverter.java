d339c7bb127adf23ae44c9b662c9dba16b49a71e
Omurbek <omke_aka@mail.ru>
2022-09-06 01:46:17 +0600

    attemp1

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int revers = 0;
+        while (number !=0){
+            int digit = number % 10;
+            revers = revers * 10 + digit;
+            number /= 10;
+        }
+        System.out.println(revers);
     }
 }
